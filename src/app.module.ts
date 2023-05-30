2bff95979655409a7566fce6081f05d21a0a213a
Brother-Yang <450028105@qq.com>
2023-05-30 16:23:01 +0800

    fix: 解决报错EntityMetadataNotFound: No metadata for Users was found.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { Users } from './users/users.entity';
 
 @Dependencies(DataSource)
 @Module({
@@ -16,8 +17,9 @@ import { UsersModule } from './users/users.module';
       username: 'root',
       password: '12345678',
       database: 'test',
-      autoLoadEntities: true, // 自动链接被 forFeature 注册的实体
-      synchronize: true, // 实体与表同步 调试模式下开始。不然会有强替换导致数据丢是
+      // autoLoadEntities: true, // 自动链接被 forFeature 注册的实体
+      // synchronize: true, // 实体与表同步 调试模式下开始。不然会有强替换导致数据丢是
+      entities: [Users],
     }),
     AuthModule,
     UsersModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -25,36 +25,3 @@ export class UsersService {
     return this.userRepository.delete(id);
   }
 }
-
-// import { Injectable } from '@nestjs/common';
-
-// export type User = any;
-
-// @Injectable()
-// export class UsersService {
-//   private readonly users: User[];
-
-//   constructor() {
-//     this.users = [
-//       {
-//         id: '1111',
-//         username: 'john',
-//         password: 'changeme',
-//       },
-//       {
-//         id: '222222',
-//         username: 'chris',
-//         password: 'secret',
-//       },
-//       {
-//         id: '33333',
-//         username: 'maria',
-//         password: 'guess',
-//       },
-//     ];
-//   }
-
-//   async findOne(username: string): Promise<User | undefined> {
-//     return this.users.find((user) => user.username === username);
-//   }
-// }

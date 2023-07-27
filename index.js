a9ca0debb3697f3b7b8ed8deb888fb0a0f1c8a6d
Brother-Yang <450028105@qq.com>
2023-07-27 15:42:19 +0800

    feat: node初始化

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+const LoginRouter = require('./router/login');
+const CookieRouter = require('./router/cookie');
+
+app.use(cors());
+app.use(express.json());
+// app.use(express.static('./public'));
+app.use(cookieParser('secret'));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.use('/login', LoginRouter);
+app.use('/cookie', CookieRouter);
+
+app.listen(3456, () => {
+  console.log('运行成功');
+});
